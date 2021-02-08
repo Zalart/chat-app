9a7e3f7b871736a3a4e885635ee103e66b39624a
artur <me@zalart.com>
2021-02-08 13:16:17 +0300

    User card completed

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,22 @@
 import './Contact.css';
 
+const source = 'https://randomuser.me/api/portraits/women/81.jpg';
+const name = 'Emily Murphy';
+const isOnline = true;
 function Contact() {
     return (
+        <div className="Contact">
+            <img className="avatar" src={source} alt="Avatar" />
+            <div>
+                <div className="name">{name}</div>
+                <div className="status">
+                    <span className={isOnline ? 'status-online' : 'status-offline'}></span>
+                    <span className="status-text">{isOnline ? 'Online' : 'Offline'}</span>
+                </div>
+            </div>
 
 
-        
+        </div>
     );
 }
-
 export default Contact;
